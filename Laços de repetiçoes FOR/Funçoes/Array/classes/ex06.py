b9f5a1600c1453fec58b0c9b67bba103b59b3d8d
Gleibson Vinicius Florêncio Nogueira <168869495+Gleibsonvinicius@users.noreply.github.com>
2025-05-01 20:56:39 +0000

    correçao da atividade

--- Laços de repetiçoes FOR/Funçoes/Array/classes/ex06.py
+++ Laços de repetiçoes FOR/Funçoes/Array/classes/ex06.py
@@ -5,35 +5,32 @@ os.system ("cls||clear")
 
 @dataclass
 
-class nome:
+class Nome:
     nome: str
     autor: str
-    Categoria: str
-    preco:float
+    categoria: str
+    preco: float
 
-
-
-def exibir_dados(self):
-    print(f"nome:{self.nome} , autor: {self.autor},categoria:{self.categoria},preco:{self.preco}" )
-
+    def exibir_dados(self):
+        print(f"Nome: {self.nome}, Autor: {self.autor}, Categoria: {self.categoria}, Preço: {self.preco}")
 
 
 lista_de_nomes = []
-Quantidade_de_autor = 2
+lista_de_autor = []
 lista_de_categoria = []
-preco = 0
+lista_de_preco = []
 
 
-for i in range(lista_de_nomes):
-    nome = nome (
-        nome = input("Digite seu Nome: "),
-    autor = int(input("Digite seu autor preferido: ")),
-    Categoria = int (input("Digite a sua categoria Favorita: ")), 
-   preco =   float(input("Digite o preco: ")), 
+quantidade = int(input("Quantos Itens deseja adicionar: "))
+
+for i in range(quantidade):
+    nome = Nome(
+    nome = input(" = Digite seu Nome: = "),
+    autor = input("= Digite seu autor preferido: = "),
+    categoria = input(" = Digite a sua categoria favorita: = "),
+    preco = float(input(" = Digite o preço: =  ")),
+
     )
-
-
-    lista_de_nomes.append(nome)  
-
-   
-    nome.exibir_dados()  
+    
+    lista_de_nomes.append(nome)
+    nome.exibir_dados()
